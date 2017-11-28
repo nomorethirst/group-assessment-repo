6782ae4ed98736444dd1c5f68083fabc2fc7bb18
unknown <kovartravis@gmail.com>
2017-11-28 09:19:46 -0600

    travis: added hashtag service

---
+++ src/app/services/hashtag.service.js
@@ -0,0 +1,21 @@
+let HOST = 'localhost'
+let PORT = 8080
+
+export class HashtagService{
+
+    constructor($log, $http){
+        'ngInject'
+        this.http = $http
+        this.log = $log
+        $log.log('hello from HashtagService!')
+        this.getByLabel('hello')
+    }
+
+    getAll(){
+        return this.http.get('http://' + HOST + ':' + PORT + '/tags/')
+    }
+
+    getByLabel(label){
+        return this.http.get('http://' + HOST + ':' + PORT + '/tags/' + label + )
+    }
+}
