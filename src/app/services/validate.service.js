dd88b2344b401c80a91027a659cf46b15a3d7438
unknown <kovartravis@gmail.com>
2017-11-28 15:31:52 -0600

    added check creds endpoint

--- src/app/services/validate.service.js
+++ src/app/services/validate.service.js
@@ -8,6 +8,7 @@ export class ValidateService{
         this.http = $http
         this.log = $log
         $log.log('hello from ValidateService!')
+        this.getCredentialsCheck({username:'a', password:'a'})
     }
 
     getTagExists(label){
