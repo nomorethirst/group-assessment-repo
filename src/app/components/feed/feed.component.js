26aadb778499eba04d117fea71aefe84db05156e
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-11-28 20:37:43 -0600

    database inclusion

--- src/app/components/feed/feed.component.js
+++ src/app/components/feed/feed.component.js
@@ -3,51 +3,40 @@ import templateUrl from 'app/components/feed/feed'
 
 const controller =
   class FeedController {
-    constructor($log, $state, userService, tweetService, $sce) {
+    constructor($log, $state, userService, tweetService, $sce, $scope) {
       'ngInject'
       this.userService = userService
       this.$state = $state
       this.tweetService = tweetService
-      let tempTweets = [{
-          "id": "1",
-          "author": "swagdaddy",
-          "posted": "123456789",
-          "content": "This is a super #cool tweet",
-          "inReplyTo": {},
-          "repostOf": {}
-        },
-        {
-          "id": "2",
-          "author": "swagdaddy",
-          "posted": "123456789",
-          "content": "Tweet number 2 baby!",
-          "inReplyTo": {},
-          "repostOf": {}
-        },
-        {
-          "id": "3",
-          "author": "swagdaddy",
-          "posted": "123456789",
-          "content": "One more time for the #fans of the #world",
-          "inReplyTo": {},
-          "repostOf": {}
-        }
-      ]
-      let position = 0;
-      tempTweets.forEach(x => {
-        let builder = "<p>"
-        x.content.split(" ").forEach(y => builder += y.startsWith("#") ? y.replace("#", "<a href='hashtag/") + "'>" + y + "</a> " : y + " ")
-        builder += "</p>"
-        console.log(builder)
-        tempTweets[position].content = $sce.trustAsHtml(builder);
-        console.log(tempTweets[position])
-        position++;
+      this.$scope = $scope
+
+      if (!this.userService.isAuthenticated()) {
+        $state.go('welcome')
+        this.logger.log('ft-profile: user not authenticated, redirecting to feed')
+      } else {
+        this.user = this.userService.user
+        this.credentials = this.userService.credentials
+      }
+      userService.getFeed().then((response) => {
+        let tweets = response.data
+        console.log($scope.tweets)
+        $scope.tweets = response.data;
+        let position = 0;
+        tweets.forEach(x => {
+          let builder = "<p>"
+          x.content.split(" ").forEach(y => builder += y.startsWith("#") ? y.replace("#", "<a href='hashtag/") + "'>" + y + "</a> " : y + " ")
+          builder += "</p>"
+          $scope.tweets[position].content = $sce.trustAsHtml(builder);
+          position++;
+        })
       })
       this.newTweetText = ""
-      this.tweets = tempTweets
     }
 
-    newTweet = () => console.log(this.newTweetText)
+    newTweet = () => this.tweetService.post({
+      "content": this.newTweetText,
+      "credentials": this.credentials
+    })
   }
 
 export const ftFeed = {
